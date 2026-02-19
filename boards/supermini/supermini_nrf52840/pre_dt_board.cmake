e8444cfc51058307b24ef6c6b93524bb5f161028
Ethan Nguyen <dev.ethan.nguyen@gmail.com>
2026-02-19 01:32:34 +0100

    feat(firmware): add SuperMini nRF52840 board definition

---
+++ boards/supermini/supermini_nrf52840/pre_dt_board.cmake
@@ -0,0 +1,4 @@
+# Suppress overlapping address warnings for nRF52840:
+# - power@40000000 & clock@40000000 & bprot@40000000
+# - acl@4001e000 & flash-controller@4001e000
+list(APPEND EXTRA_DTC_FLAGS "-Wno-unique_unit_address_if_enabled")
